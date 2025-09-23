575f15bd43d3c10dce27f6ae48f3da553f13424a
Isabella Duan <isadua28@bergen.org>
2025-09-23 16:03:59 -0400

    actual bmi calculator completed

--- Lesson03-Ternary-Operator/bmi-calculator.py
+++ Lesson03-Ternary-Operator/bmi-calculator.py
@@ -1,8 +1,17 @@
-height = int(input("Enter your height in inches: "))
-weight = int(input("Enter your weight in pounds: "))
-bmi = (weight / (height**2)) * 703
+print("=== BMI Calculator ===")
+height = float(input("Enter your height (in): "))
+weight = float(input("Enter your weight (lbs): "))
+print("")
 
-if height and weight:
+print("=== BMI Health Report ===")
+print("")
+
+bmi = round((weight / (height**2)) * 703, 1)
+
+print(f'Height: {height}"')
+print(f"Weight: {weight} lbs")
+print(f"BMI: {bmi}")
+if height > 0.0 and weight > 0.0:
     category = ("Underweight" if bmi < 18.5 else 
                 "Normal" if 18.5 <= bmi <= 25 else
                 "Overweight" if 25 <= bmi < 30 else
@@ -10,4 +19,8 @@ if height and weight:
 else: 
     print("Enter valid height or weight")
     
-print(f"You are {category}")
+print(f"Category: {category}")
+rec = "Gain / Maintain weight" if bmi <= 25 else "Lose weight"
+health_risk = "Low" if bmi <= 25 else "Moderate / High"
+print(f"Recommendation: {rec}")
+print(f"Health Risk: {health_risk}")
